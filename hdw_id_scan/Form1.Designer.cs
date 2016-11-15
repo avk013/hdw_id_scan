661a4d8455863da2985e10087b4b3b2d6df9141a
Kovalenko Alexander <avk013@live.ru>
2016-11-15 06:57:04 +0200

    добавлен HDD, его серийник в кодах символов и странной последовательностью, каждый 2 буквы нужно менять местами

--- hdw_id_scan/Form1.Designer.cs
+++ hdw_id_scan/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // listBox1
@@ -37,7 +38,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(1, 0);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(514, 56);
+            this.listBox1.Size = new System.Drawing.Size(514, 212);
             this.listBox1.TabIndex = 0;
             // 
             // button1
@@ -50,16 +51,26 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 218);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 228);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 273);
+            this.ClientSize = new System.Drawing.Size(510, 447);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "hdw_id_scan";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- hdw_id_scan/Form1.cs
+++ hdw_id_scan/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Management;
+using System.Net.NetworkInformation;
 
 namespace hdw_id_scan
 {
@@ -13,8 +14,9 @@ namespace hdw_id_scan
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
             ManagementObjectSearcher searcher = new ManagementObjectSearcher
-              ("SELECT Product, SerialNumber FROM Win32_BaseBoard");
+              ("SELECT Manufacturer, Product, SerialNumber FROM Win32_BaseBoard");
 
             ManagementObjectCollection information = searcher.Get();
             foreach (ManagementObject obj in information)
@@ -22,14 +24,45 @@ namespace hdw_id_scan
                 foreach (PropertyData data in obj.Properties)
                     listBox1.Items.Add(string.Format("{0} = {1}", data.Name, data.Value));
             }
-            searcher = new ManagementObjectSearcher ("SELECT Name, ProcessorId FROM Win32_Processor");
+            searcher = new ManagementObjectSearcher ("SELECT Name, MaxClockSpeed, ProcessorId FROM Win32_Processor");
             information = searcher.Get();
             foreach (ManagementObject obj in information)
             {
                 foreach (PropertyData data in obj.Properties)
                     listBox1.Items.Add(string.Format("{0} = {1}", data.Name, data.Value));
             }
-            // listBox1.Items.Add(Environment.)
+            searcher = new ManagementObjectSearcher("SELECT Caption, AdapterRAM FROM Win32_VideoController");
+            information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                    listBox1.Items.Add(string.Format("{0} = {1}", data.Name, data.Value));
+            }
+            /////////
+            searcher = new ManagementObjectSearcher("SELECT MacAddress FROM Win32_NetworkAdapterConfiguration");
+            information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                    if (string.Format("{0}", data.Value)!="") listBox1.Items.Add(string.Format("{0} = {1}", data.Name, data.Value.ToString().Replace(":", "")));
+            }
+          searcher = new ManagementObjectSearcher("SELECT Model, Size, SerialNumber FROM Win32_DiskDrive");
+        //  searcher = new ManagementObjectSearcher("SELECT SerialNumber FROM Win32_PhysicalMedia");
+            information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                {
+                    listBox1.Items.Add(string.Format("{0} = {1}", data.Name, data.Value.ToString().Replace(":", "")));
+                    textBox1.Text += string.Format("{0} = {1}", data.Name, data.Value.ToString().Replace(":", ""))+Environment.NewLine;
+                }
+
+            }
+
+
+
+            //////////
+            // listBox1.Items.Add(MACAddress.ToString());
         }
     }
 }
